083a91638a147ea359c8b473f285225b8ac979d4
Derick Ruiz <derick.r.ruiz@gmail.com>
2025-05-14 01:47:53 +0700

    Some custom js to move the position of the method pill in the sidebar. Also adding an icon to the activity page for the sim APIs.

---
+++ custom.js
@@ -0,0 +1,9 @@
+// Select all .method-nav-pill elements inside <a> tags
+document.querySelectorAll('a .method-nav-pill').forEach(pill => {
+    const a = pill.closest('a');
+    if (a) {
+      // Remove the pill from its current position and append it as the last child of <a>
+      pill.remove();
+      a.appendChild(pill);
+    }
+  });
