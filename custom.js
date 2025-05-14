655732ebdd0dc66bb5abfdfd1b6143ef80a33f36
Derick Ruiz <derick.r.ruiz@gmail.com>
2025-05-14 15:50:48 +0700

    Custom JS for changing the position of the method pills to the right side of the navbar. moving images to the appropriate places in the images/ folder and updating the correct mdx files. changing the names of the svm endpoints so it's cleaner. Uploading custom icons for each of the evm endpoints.

--- custom.js
+++ custom.js
@@ -2,8 +2,8 @@
 document.querySelectorAll('a .method-nav-pill').forEach(pill => {
     const a = pill.closest('a');
     if (a) {
-      // Remove the pill from its current position and append it as the last child of <a>
-      pill.remove();
-      a.appendChild(pill);
+        // Remove the pill from its current position and append it as the last child of <a>
+        pill.remove();
+        a.appendChild(pill);
     }
-  });
+});
